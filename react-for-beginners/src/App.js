621caa0988f6b24b72b095c96dfaa3c73b88c16b
Jeongmin CHOI <jumin0114@gmail.com>
2023-10-04 15:10:32 +0900

    ReactToDolist

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -3,27 +3,37 @@ import style from "./App.module.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo == "") {
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo(""); //state를 직접적으로 수정하지 않는다
 
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+  };
 
-  useEffect(() => {
-    
-  }, []);
-  useEffect(() => {
-    
-  }, [keyword]);
   return (
     <div>
-      <input 
-        value={keyword}
-        onChange={onChange} 
-        type="text" 
-        placeholder="Search.." />
-      <h1 className={style.title}>{counter}</h1>
-      <button onClick={onClick}>Click Me.</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write to do"
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr/>
+      <ul>
+      {toDos.map((item, index) => (
+      <li key={index}>{item}</li>
+      ))}
+      </ul>
     </div>
   );
 }
