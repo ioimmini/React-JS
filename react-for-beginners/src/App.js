405c377a5cf89bd9ee42cdfecb16182ce525aed9
Jeongmin CHOI <jumin0114@gmail.com>
2023-10-04 15:36:14 +0900

    ReactToDolist

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -15,6 +15,10 @@ function App() {
     setToDo(""); //state를 직접적으로 수정하지 않는다
 
   };
+  const deleteBtn = index => {
+    setToDos(toDos.filter((item, toDoIndex) => index !== toDoIndex));
+    };
+    
 
   return (
     <div>
@@ -26,12 +30,14 @@ function App() {
           type="text"
           placeholder="Write to do"
         />
-        <button>Add To Do</button>
+        <button>Add</button>
       </form>
       <hr/>
       <ul>
       {toDos.map((item, index) => (
-      <li key={index}>{item}</li>
+      <li key={index}>{item}
+      <button  onClick={() => deleteBtn(index)}>X</button>
+      </li>
       ))}
       </ul>
     </div>
