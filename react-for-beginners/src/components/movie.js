731a150349684849ced8a8196e8501cd1d26b5a7
Jeongmin CHOI <jumin0114@gmail.com>
2023-12-13 15:01:36 +0900

    CSS 수정

--- react-for-beginners/src/components/movie.js
+++ react-for-beginners/src/components/movie.js
@@ -1,18 +1,22 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css"
 
 function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
-      <p>{year}</p>
-      <ul>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <h2 className={styles.movie__title}>
+        <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+      <p className={styles.movie__year}>{year}</p>
+      <ul className={styles.movie__genres}>
         {genres.map((g) => (
           <li key={g}>{g}</li>
         ))}
       </ul>
-      <p>{summary}</p>
+      <p>
+        {summary.length > 236 ? `${summary.slice(0, 235)}...`: summary}</p>
     </div>
   );
 }
--- react-for-beginners/src/routes/Home.js
+++ react-for-beginners/src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie.js";
+import styles from "./Home.module.css";
 
 function Home(){
   const [loading, setLoading] = useState(true);
@@ -18,11 +19,11 @@ function Home(){
   }, []);
 
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
